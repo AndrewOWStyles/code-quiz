fff14ecab1ee2b3a5aa9f29e80433e3defe7e1fb
AndrewOWStyles <61656980+AndrewOWStyles@users.noreply.github.com>
2023-01-14 15:00:50 +0000

    Add files via upload

---
+++ starter/assets/scripts/logic.js
@@ -0,0 +1,7 @@
+let timer = 90;
+let intervalId = setInterval(function(){
+    timer--
+},1000)
+
+// timer add ( timer += "number here")
+// timer sub ( timer -= "number here")
---
+++ starter/assets/scripts/questions.js
@@ -0,0 +1,32 @@
+let questions = [
+    {//put choice string in button
+        questionTitle: "question 1",
+        Choices: ["ex1", "ex2", "ex3", "ex4"],
+        correctAnswer: 1,
+    },
+    {
+        questionTitle: "question 2",
+        Choices: ["ex1", "ex2", "ex3", "ex4"],
+        correctAnswer: 2,
+    },
+    {
+        questionTitle: "question 3",
+        Choices: ["ex1", "ex2", "ex3", "ex4"],
+        correctAnswer: 0,
+    },
+    {
+        questionTitle: "question 4",
+        Choices: ["ex1", "ex2", "ex3", "ex4"],
+        correctAnswer: 3,
+    },
+]
+let choicesE1 = document.querySelector("#choices")
+
+let currentQuestion = 0;
+// give data-index to choices button so when button is clicked we can see what index it is
+
+choicesE1.addEventListener("click", function(){
+    
+
+
+})
